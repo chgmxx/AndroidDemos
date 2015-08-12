f667f587a0e37c0fd7fe042035060ceaf5f03f45
XuDing <ahtcfg24@163.com>
2015-08-12 20:44:30 +0800

    Start Service

---
+++ startservicedemo/src/main/java/com/example/ahtcfg24/startservicedemo/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.ahtcfg24.startservicedemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+/**
+ * 启动Service
+ */
+public class MainActivity extends Activity
+{
+    private Button button;
+    private Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new ButtonListener());
+        button2.setOnClickListener(new ButtonListener());
+    }
+
+
+    private class ButtonListener implements OnClickListener
+    {
+        @Override
+        public void onClick(View v)
+        {
+            Intent intent = new Intent(MainActivity.this, ServiceDemo.class);
+            switch (v.getId())
+            {
+                case R.id.button:
+                    startService(intent);//启动Service
+                    break;
+                case R.id.button2:
+                    stopService(intent);//终止Service
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ startservicedemo/src/main/java/com/example/ahtcfg24/startservicedemo/ServiceDemo.java
@@ -0,0 +1,42 @@
+package com.example.ahtcfg24.startservicedemo;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class ServiceDemo extends Service
+{
+    public static final String TAG = "ServiceDemo";
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+
+        Log.i(TAG, "--->onCreate");
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        Log.i(TAG, "--->onDestroy");
+
+
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId)
+    {
+        super.onStart(intent, startId);
+        Log.i(TAG, "--->onStart");
+    }
+
+    /*onBinder()是父类中的抽象方法，必须被复写*/
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        return null;
+    }
+}
