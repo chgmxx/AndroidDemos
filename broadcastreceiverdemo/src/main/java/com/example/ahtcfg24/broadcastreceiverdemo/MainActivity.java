df353e01a44d1706406a0207721520c699112d45
XuDing <ahtcfg24@163.com>
2015-08-16 21:48:48 +0800

    static receiver

--- broadcastreceiverdemo/src/main/java/com/example/ahtcfg24/broadcastreceiverdemo/MainActivity.java
+++ broadcastreceiverdemo/src/main/java/com/example/ahtcfg24/broadcastreceiverdemo/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.ahtcfg24.broadcastreceiverdemo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send();
+            }
+        });
 
     }
 
+    public void send() {
+        Intent intent = new Intent("android.intent.action.My_Broadcast");
+        intent.putExtra("广播", "发出一条广播");
+        sendBroadcast(intent);
+    }
 
 }
